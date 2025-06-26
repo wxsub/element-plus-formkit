ff9f767ce1d59116d5d44a84d1e9399ca5faef29
NicolasHome <1556363381@qq.com>
2025-06-26 16:21:26 +0800

    The select module is refactored with vue3 setup syntactic sugar.

--- src/modules/select.vue
+++ src/modules/select.vue
@@ -2,30 +2,33 @@
     <el-select v-model="_value" v-bind="$attrs">
         <el-option
                 v-for="it in options"
-                :key="it[$attrs.valueKey || 'id']"
-                :label="it[$attrs.labelKey || 'name']"
-                :value="it[$attrs.valueKey || 'id']">
+                :key="it[$attrs?.valueKey || 'id']"
+                :label="it[$attrs?.labelKey || 'name']"
+                :value="it[$attrs?.valueKey || 'id']">
             <div v-html="it[$attrs.labelKey || 'name']"/>
         </el-option>
     </el-select>
 </template>
 
-<script>
-export default {
-    name: 'FormKitSelect',
-    model: { prop: 'value', event: 'change' },
-    props: {
-        value: { default: null },
-        options: { type: Array, default: () => [] }
+<script setup lang="ts">
+import { ElSelect, ElOption } from 'element-plus'
+
+const props = defineProps({
+    modelValue: { default: null },
+    options: { type: Array<any>, default: () => [] }
+})
+
+const emit = defineEmits(['update:modelValue']);
+
+const $attrs = useAttrs() as { valueKey?: string; labelKey?: string };
+
+const _value: any = computed({
+    get: () => {
+        const { multiple } = useAttrs() || {}
+        return props.modelValue || (multiple ? [] : null)
     },
-    computed: {
-        _value: {
-            get() {
-                const { multiple } = this.$attrs || {}
-                return this.value || (multiple ? [] : null)
-            },
-            set(value) { this.$emit('change', value) }
-        }
+    set: (value) => {
+        emit('update:modelValue', value)
     }
-}
+})
 </script>
