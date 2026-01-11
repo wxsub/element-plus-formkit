e9153302611214557817a2673e0b6c279c057a9d
NicolasHome <1556363381@qq.com>
2026-01-11 11:56:09 +0800

    docs: Added viewport meta tag to optimize mobile display.

--- pages/vuepress.config.ts
+++ pages/vuepress.config.ts
@@ -101,5 +101,8 @@ export default defineUserConfig({
     },
   },
   base: '/element-plus-formkit/',
-  head: [['link', { rel: 'icon', href: 'https://raw.githubusercontent.com/wxsub/element-plus-formkit/refs/heads/main/pages/docs/public/logo.png' }]]
+  head: [
+    ['link', { rel: 'icon', href: 'https://raw.githubusercontent.com/wxsub/element-plus-formkit/refs/heads/main/pages/docs/public/logo.png' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' }]
+  ]
 })
