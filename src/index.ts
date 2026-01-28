5900a4dcf4482f83e0de1b4aca04e28931875bf7
NicolasHome <1556363381@qq.com>
2026-01-28 21:05:08 +0800

    Add address module and update upload import/export
    Introduced the address module and updated the upload module to use lowercase naming. Both modules are now exported individually. Removed Upload from the pluginObject to streamline module management.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import formkit from '@/formkit.vue';
-import Upload from '@/modules/upload.vue';
+import upload from '@/modules/upload.vue';
+import address from '@/modules/address.vue';
 
 import { setConfigure, getConfigure, type configType } from '@/config';
 import { modules, registerModule, type CustomModuleRegistry } from '@/module-registry';
@@ -14,8 +15,6 @@ export { registerModule, modules }
 export type { configType, CustomModuleRegistry }
 
 const pluginObject: ElementPlusFormkitPlugin = {
-  Upload,
-  
   setConfigure,
   getConfigure,
   
@@ -24,5 +23,7 @@ const pluginObject: ElementPlusFormkitPlugin = {
 };
 
 export { pluginObject }
+
+export { upload, address }
 
 export default formkit;
