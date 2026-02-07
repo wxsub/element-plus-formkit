a86625add6fa3ef90f432589a3a4f5182ff9d781
NicolasDevil <1556363381@qq.com>
2026-02-07 01:39:39 +0000

    Deploying to ci-doc-pages from @ wxsub/element-plus-formkit@90e2e93ca58c9d77f143a7fa56cb1e2651facea8 🚀

---
+++ assets/customModule-Cmfy-LOU.js
@@ -0,0 +1,1 @@
+import{d as e,c as o,o as t}from"./app-Dfdb0854.js";const u=e({__name:"customModule",props:{value:{type:Object,default:()=>({})}},setup(s){return(c,a)=>(t(),o("h2",null,"This is customModule"))}});export{u as default};
